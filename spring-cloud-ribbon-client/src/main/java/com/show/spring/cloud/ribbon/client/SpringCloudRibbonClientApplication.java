8fe0a95103af1e88b47678694e4cf99e2cef4a2f
主打情歌 <1004108488@qq.com>
2019-06-06 23:17:33 +0800

    SpringCloudRibbonClient 项目初始化

---
+++ spring-cloud-ribbon-client/src/main/java/com/show/spring/cloud/ribbon/client/SpringCloudRibbonClientApplication.java
@@ -0,0 +1,14 @@
+package com.show.spring.cloud.ribbon.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRibbonClientApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringCloudRibbonClientApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-ribbon-client/src/test/java/com/show/spring/cloud/ribbon/client/SpringCloudRibbonClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.show.spring.cloud.ribbon.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudRibbonClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
