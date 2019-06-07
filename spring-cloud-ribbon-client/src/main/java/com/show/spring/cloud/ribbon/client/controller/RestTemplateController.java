524230da271275fda0b0da7f7e0476f67dad71b6
主打情歌 <1004108488@qq.com>
2019-06-07 23:40:44 +0800

    添加类

---
+++ spring-cloud-ribbon-client/src/main/java/com/show/spring/cloud/ribbon/client/controller/RestTemplateController.java
@@ -0,0 +1,9 @@
+package com.show.spring.cloud.ribbon.client.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/RestTemplate")
+public class RestTemplateController {
+}
