427cce27c1bd75c1d286069dedb26d8285a644ac
主打情歌 <1004108488@qq.com>
2019-06-07 23:42:07 +0800

    添加类

--- spring-cloud-ribbon-client/src/main/java/com/show/spring/cloud/ribbon/client/controller/RestTemplateController.java
+++ spring-cloud-ribbon-client/src/main/java/com/show/spring/cloud/ribbon/client/controller/RestTemplateController.java
@@ -3,6 +3,11 @@ package com.show.spring.cloud.ribbon.client.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 演示 RestTemplate 的用法
+ * @author show
+ * @date 23:41 2019/6/7
+ */
 @RestController
 @RequestMapping("/RestTemplate")
 public class RestTemplateController {
