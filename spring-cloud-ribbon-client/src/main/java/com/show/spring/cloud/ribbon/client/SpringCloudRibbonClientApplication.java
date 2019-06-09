bbb05fb0c72a30502d79467eb60cef303f0699a3
主打情歌 <1004108488@qq.com>
2019-06-09 16:46:14 +0800

    Feign 请求其他微服务实现

--- spring-cloud-ribbon-client/src/main/java/com/show/spring/cloud/ribbon/client/SpringCloudRibbonClientApplication.java
+++ spring-cloud-ribbon-client/src/main/java/com/show/spring/cloud/ribbon/client/SpringCloudRibbonClientApplication.java
@@ -2,8 +2,10 @@ package com.show.spring.cloud.ribbon.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringCloudRibbonClientApplication {
 
     public static void main(String[] args) {
