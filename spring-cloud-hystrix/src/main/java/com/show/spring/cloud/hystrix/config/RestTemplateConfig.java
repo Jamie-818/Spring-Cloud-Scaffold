ced5626dee7102d3e60194f0db49f0707e040425
主打情歌 <1004108488@qq.com>
2019-06-12 16:34:41 +0800

    修改 bean 位置

--- spring-cloud-hystrix/src/main/java/com/show/spring/cloud/hystrix/config/RestTemplateConfig.java
+++ spring-cloud-hystrix/src/main/java/com/show/spring/cloud/hystrix/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.show.spring.cloud.hystrix;
+package com.show.spring.cloud.hystrix.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
