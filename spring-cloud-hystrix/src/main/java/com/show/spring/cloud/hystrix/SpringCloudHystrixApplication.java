6a115eb492774bbea09e9f11e7f0375a9d6e129a
主打情歌 <1004108488@qq.com>
2019-06-12 16:31:03 +0800

    添加 Hystrix 服务降级 逻辑

--- spring-cloud-hystrix/src/main/java/com/show/spring/cloud/hystrix/SpringCloudHystrixApplication.java
+++ spring-cloud-hystrix/src/main/java/com/show/spring/cloud/hystrix/SpringCloudHystrixApplication.java
@@ -1,13 +1,17 @@
 package com.show.spring.cloud.hystrix;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
  * 启动类
- * @author xuanweiyao
+ * @author show
+ * @SpringCloudApplication 里面包含下面三个注解
+ * - @SpringBootApplication Springboot项目启动注解
+ * - @EnableDiscoveryClient 注册中心Client端启动注解
+ * - @EnableCircuitBreaker  断路器注解
  */
-@SpringBootApplication
+@SpringCloudApplication
 public class SpringCloudHystrixApplication {
 
     public static void main(String[] args) {
---
+++ spring-cloud-hystrix/src/main/java/com/show/spring/cloud/hystrix/controller/HystrixController.java
@@ -0,0 +1,84 @@
+package com.show.spring.cloud.hystrix.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.show.spring.cloud.hystrix.fallback.HystrixFallback;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 演示Hystrix
+ * @author show
+ * @date 11:10 2019/6/12
+ */
+@RestController
+@RequestMapping("/Hystrix")
+@Slf4j
+public class HystrixController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 演示请求其他微服务 Hystrix 处理
+     * @author show
+     * @date 15:09 2019/6/12
+     * @HystrixCommand 指定异常时执行什么方式
+     */
+    @HystrixCommand(fallbackMethod = "ribbonErrorFallback")
+    @GetMapping("/ribbonError")
+    public Map ribbonError() {
+        // 该URL请求为404
+        String url = "http://Server/error";
+        Map responseData = restTemplate.getForObject(url, Map.class);
+        log.info("返回值为:{}", responseData);
+        return responseData;
+    }
+
+    /**
+     * 演示 代码异常 Hystrix 处理
+     * @author show
+     * @date 15:09 2019/6/12
+     * @HystrixCommand 指定异常时执行什么方式
+     */
+    @HystrixCommand(fallbackMethod = "runtimeErrorFallback")
+    @GetMapping("/runtimeError")
+    public Map runtimeError() {
+
+        throw new RuntimeException("发生异常");
+    }
+
+    /**
+     * 代码异常返回
+     * @author show
+     * @date 15:11 2019/6/12
+     */
+    private Map ribbonErrorFallback() {
+
+        Map<String, Object> fallbackMap = new HashMap<>(16);
+        fallbackMap.put("status", "99");
+        fallbackMap.put("message", "断路器降级启动，请求异常");
+        fallbackMap.put("data", "");
+        return fallbackMap;
+    }
+
+    /**
+     * 请求异常返回
+     * @author show
+     * @date 15:10 2019/6/12
+     */
+    private Map runtimeErrorFallback() {
+
+        Map<String, Object> fallbackMap = new HashMap<>(16);
+        fallbackMap.put("status", "99");
+        fallbackMap.put("message", "断路器降级启动，运行异常");
+        fallbackMap.put("data", "");
+        return fallbackMap;
+    }
+}
