7a4fddb8637998a4a5e6744b4207a4516d3f37a9
主打情歌 <1004108488@qq.com>
2019-06-28 10:21:09 +0800

    整理Pom文件

---
+++ shell/win10/PowerShell启动config.cmd
@@ -0,0 +1,1 @@
+start powershell "java -jar ..\jar\spring-cloud-config-1.0.0-SNAPSHOT.jar"
---
+++ shell/win10/PowerShell启动多个eureka.cmd
@@ -0,0 +1,4 @@
+start powershell "java -jar ..\jar\spring-cloud-eureka-1.0.0-SNAPSHOT.jar --spring.profiles.active=eureka1"
+start powershell "java -jar ..\jar\spring-cloud-eureka-1.0.0-SNAPSHOT.jar --spring.profiles.active=eureka2"
+start powershell "java -jar ..\jar\spring-cloud-eureka-1.0.0-SNAPSHOT.jar --spring.profiles.active=eureka3"
+
